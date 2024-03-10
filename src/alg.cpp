026efb31da37fdc7e1b63c5586a7b33f2105a098
kkerwqss <banykinaks@yandex.ru>
2024-03-10 12:56:09 +0300

    update

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,9 +14,9 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-    if (n == 1) {
+    if (n == 1)
         return 1;
-    } else
+    else 
         return n * fact(n - 1);
 }
 
@@ -34,7 +34,7 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
     double res = 0;
-    for (uint64_t i = 1; i <= count; i++) {
+    for (int i = 1; i <= count; i++) {
         res += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
     }
     return res;
@@ -42,7 +42,7 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
     double res = 0;
-    for (uint64_t i = 1; i <= count; i++) {
+    for (int i = 1; i <= count; i++) {
         res += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
     }
     return res;
