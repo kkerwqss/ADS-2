78fc43a079fa62be4571b54eccf5166d8ad9c227
kkerwqss <banykinaks@yandex.ru>
2024-03-10 11:43:16 +0300

    fix tabs

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,50 +2,49 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-	double res = 1.0;
-	if (n == 0) {
-		return 1.0;
-	}
-	for (uint64_t i = 0; i < n; i++) {
-		res *= value;
-	}
-	return res;
+    double res = 1.0;
+    if (n == 0) {
+        return 1.0;
+    }
+    for (uint64_t i = 0; i < n; i++) {
+        res *= value;
+    }
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
-	if (n == 1) {
-		return 1;
-	}
-	else
-		return n * fact(n - 1);
+    if (n == 1) {
+        return 1;
+    }
+    else
+        return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-	return pown(x, n) / fact(n);
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-	double res = 0;
-	for (uint64_t i = 0; i <= count; i++) {
-		res += calcItem(x, i);
-	}
-	return res;
+    double res = 0;
+    for (uint64_t i = 0; i <= count; i++) {
+        res += calcItem(x, i);
+    }
+    return res;
 }
 
 double sinn(double x, uint16_t count) {
-	double res = 0;
-	for (uint64_t i = 1; i <= count; i++) {
-		res += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
-	}
-	return res;
+    double res = 0;
+    for (uint64_t i = 1; i <= count; i++) {
+        res += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
+    }
+    return res;
 }
 
 double cosn(double x, uint16_t count) {
-	double res = 0;
-	for (uint64_t i = 1; i <= count; i++) {
-		res += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
-	}
-	return res;
+    double res = 0;
+    for (uint64_t i = 1; i <= count; i++) {
+        res += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+    }
+    return res;
 }
